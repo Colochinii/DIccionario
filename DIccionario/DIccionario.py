dc547fb84abb4ea9433e4962c51248d32fdd546d
Colochinii <Colochinii@DESKTOP-V5H3U80>
2022-02-13 21:43:34 -0600

    Agregar archivos de proyecto.

---
+++ DIccionario/DIccionario.py
@@ -0,0 +1,10 @@
+## determino lo que quiero escribir en python por que es muy importante p[ara saber que andoi  programansd]
+
+def variable():
+    x=input('inngrese un valor-...>')
+    """el valor es el siguiente"""
+    print(x)
+
+
+
+
